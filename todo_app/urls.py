575103392bfb305306660cb743c418b7ccc36d68
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-17 17:30:01 +0300

    url added
    delete, yes and no finished, update urls added successful.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('',views.index, name="index"),
     path('about/',views.about, name="about"),
     path('create/',views.create, name="create"),
+    path('delete/<Todos_id>',views.delete, name="delete"),
+    path('yes_finish/<Todos_id>',views.yes_finish, name="yes_finish"),
+    path('no_finish/<Todos_id>',views.no_finish, name="no_finish"),
+    path('update/<Todos_id>',views.update, name="update"),
+    
 ]
